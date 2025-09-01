5ab713b04bd77208bedb2b1ca0889dbfb966a882
raashed <mohammedraashed10@gmail.com>
2025-09-01 22:05:39 +0530

    chore: eslintcache tests successull

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  const test1 = 'hello there';
-  const test2 = 'we here to test';
-  return <div>{`${test1} brother ${test2}`}</div>;
+  return <div>hello there</div>;
 }
